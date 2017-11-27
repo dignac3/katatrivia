f4ac5537027d3d5e926f118387dd2745f56c9c39
Dorian Dignac <dorian.dignac19@hotmail.fr>
2017-11-27 07:51:25 +0100

    Recuperation code legacy

---
+++ src/main/java/fr/unilim/iut/katatrivia/GameRunner.java
@@ -0,0 +1,34 @@
+package fr.unilim.iut.katatrivia;
+
+import java.util.Random;
+
+import fr.unilim.iut.katatrivia.Game;
+
+public class GameRunner {
+
+	private static boolean notAWinner;
+
+	public static void main(String[] args) {
+
+		Random rand = new Random();
+
+		Game aGame = new Game();
+
+		aGame.add("Chet");
+		aGame.add("Pat");
+		aGame.add("Sue");
+
+		do {
+
+			aGame.roll(rand.nextInt(5) + 1);
+
+			if (rand.nextInt(9) == 7) {
+				notAWinner = aGame.wrongAnswer();
+			} else {
+				notAWinner = aGame.wasCorrectlyAnswered();
+			}
+
+		} while (notAWinner);
+
+	}
+}
